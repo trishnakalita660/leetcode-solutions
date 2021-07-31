227ead7144d40943815408b94fe0489cc9e9218b
Trishna Kalita <54037204+trishnakalita660@users.noreply.github.com>
2021-07-31 19:33:39 +0530

    Update clone-graph.cpp
    OPTIMISED

--- clone-graph/clone-graph.cpp
+++ clone-graph/clone-graph.cpp
@@ -24,36 +24,52 @@ public:
 
 class Solution {
 public:
-    void dfs(Node* curr, Node*node, vector<Node*> &visited){
-            visited[node->val] = node;
+//     void dfs(Node* curr, Node*node, vector<Node*> &visited){
+//             visited[node->val] = node;
             
-            for(auto e: curr->neighbors){
-                     if(visited[e->val]==NULL){
-                            Node* newnode = new Node(e->val);
-                            (node->neighbors).push_back(newnode);
-                            dfs(e, newnode, visited);
-                    }
-                    else{
-                            (node->neighbors).push_back(visited[e->val]);
-                    }
-            }
-    }
+//             for(auto e: curr->neighbors){
+//                      if(visited[e->val]==NULL){
+//                             Node* newnode = new Node(e->val);
+//                             (node->neighbors).push_back(newnode);
+//                             dfs(e, newnode, visited);
+//                     }
+//                     else{
+//                             (node->neighbors).push_back(visited[e->val]);
+//                     }
+//             }
+//     }
+        
+    unordered_map<Node*, Node*> ref;
     Node* cloneGraph(Node* node) {
-        if(node==NULL) return NULL;
-            vector<Node*> visited(1000, NULL);
-            Node*copy = new Node(node->val);
+//         if(node==NULL) return NULL;
+//             vector<Node*> visited(1000, NULL);
+//             Node*copy = new Node(node->val);
             
-            visited[node->val] = copy;
-            for(auto curr: node->neighbors){
-                    if(visited[curr->val]==NULL){
-                            Node* newnode = new Node(curr->val);
-                            (copy->neighbors).push_back(newnode);
-                            dfs(curr, newnode, visited);
-                    }
-                    else{
-                            (copy->neighbors).push_back(visited[curr->val]);
-                    }
+//             visited[node->val] = copy;
+//             for(auto curr: node->neighbors){
+//                     if(visited[curr->val]==NULL){
+//                             Node* newnode = new Node(curr->val);
+//                             (copy->neighbors).push_back(newnode);
+//                             dfs(curr, newnode, visited);
+//                     }
+//                     else{
+//                             (copy->neighbors).push_back(visited[curr->val]);
+//                     }
+//             }
+//             return copy;
+            
+//             OPTIMISED
+            
+            if(!node) return NULL;
+            
+            if(ref.find(node)!= ref.end()){
+                    return ref[node];
             }
-            return copy;
+            
+            ref[node]= new Node(node->val);
+            for(auto x: node->neighbors){
+                    ref[node]->neighbors.push_back(cloneGraph(x));
+            }
+            return ref[node];
     }
 };

66005364aafd7eaf6e2047a5c4555584989755c9
Trishna Kalita <54037204+trishnakalita660@users.noreply.github.com>
2021-08-08 13:26:59 +0530

    Time: 48 ms (34.49%), Space: 10.8 MB (13.54%) - LeetHub

---
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int index=s.size();
+            unordered_map<char, pair<int,int>> map;
+            for(int i=0;i<s.size();i++){
+                    map[s[i]].first++;
+                    map[s[i]].second=i;
+            }
+            for(auto &it : map){
+                    if(it.second.first ==1){
+                            index = min(index, it.second.second);
+                    }
+            }
+            return index==s.length()?-1:index;
+            
+//             for(int i=0;i<s.size();i++){
+//                     map[s[i]].first++;
+//                     map[s[i]].second = i;
+//             }
+            
+//             for(auto &itr : map){
+//                     if(itr.second.first == 1) index = min(index, itr.second.second);
+//             }
+//             return index==s.size()?-1:index;
+        
+    }
+};

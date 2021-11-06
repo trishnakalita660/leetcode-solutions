87a834d69e01db4d6a190cae8d71bcc4323dae65
Trishna Kalita <54037204+trishnakalita660@users.noreply.github.com>
2021-11-06 19:59:29 +0530

    Time: 0 ms (100.00%), Space: 6 MB (52.87%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) 
+{
+     int steps[n];
+            if(n<=1)  return n;
+   else    steps[0]=1;
+           steps[1]=2;
+     
+    for(int i=2;i<n;i++)
+     {
+         steps[i]=steps[i-2]+steps[i-1];
+     }
+     return steps[n-1];
+ }
+};
+ 

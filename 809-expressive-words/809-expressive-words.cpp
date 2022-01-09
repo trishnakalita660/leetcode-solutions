f8c1e5867a20ab7971cabb8272a4953e6a5cc7f8
Trishna Kalita <54037204+trishnakalita660@users.noreply.github.com>
2022-01-09 18:10:59 +0530

    Time: 8 ms (41.60%), Space: 7.3 MB (99.73%) - LeetHub

--- 809-expressive-words/809-expressive-words.cpp
+++ 809-expressive-words/809-expressive-words.cpp
@@ -1,41 +1,34 @@
- 
 class Solution {
 public:
-   // check for each token/sub-word
-    bool help(string str1, string str2) {
-        if(str1[0] != str2[0]) return false;
-        if(str1.size()==str2.size()) return true;
-        if(str2.size() > str1.size()) return false;
-        if(str2.size()==1 && str1.size()==2) return false;
-        return true;
-    }
-    // split of each word with same repeated char
-    bool solve(string s, string word) {
-        int i = 0, j = 0;
-        while(i<s.size() && j<word.size()) {
-            string str1 = "";
-            string str2 = "";
-            char ch1 = s[i];
-            char ch2 = word[j];
-            while(ch1 == s[i]) {
-                str1 += s[i];
-                i++;
-            }
-            while(ch2 == word[j]) {
-                str2 += word[j];
-                j++;
-            }
-            if(!help(str1,str2)) return false;
-        }
-        if(i!=s.size() || j!=word.size()) return false;
-        return true;
-    }
-    // main function
     int expressiveWords(string s, vector<string>& words) {
+    
         int ans = 0;
-        for(auto x : words){
-            if(solve(s,x)) ans++;
+        for(auto&w: words)
+        {
+            int j = 0,  i = 0;
+            int cnt = 0;
+            if (s.length() < w.length()) continue;
+            while (i < s.length() && j < w.length())
+            {
+                cnt = 0;
+                int k = i;
+                while (i < s.length() && j < w.length() && w[j] == s[i] && s[i] == s[k])
+                {
+                    
+                    ++cnt;
+                    ++i;
+                    ++j;
+                }
+                if (cnt == 0) break;
+                 k = --i;
+                while (k +1  < s.length() && s[k +1] == s[i]) ++k;
+                if (k != i && cnt + k - i < 3) break;
+                i = ++k;
+            }
+            ans += (i == s.length() && j == w.length());
         }
         return ans;
+        
+        
     }
 };
